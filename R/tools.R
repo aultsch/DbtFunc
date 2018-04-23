138c41ae5330bd77a57d8203f3b7e5d1d41c5679
Florian Lerch <florianlerch@swla3242.WLAN.Uni-Marburg.DE>
2018-04-23 10:03:30 +0200

    added qq - related methods

--- R/tools.R
+++ R/tools.R
@@ -1,5 +1,4 @@
-slog <- function (x, m = 10)
-{
+slog <- function (x, m = 10){
   absX = abs(x)
   s = sign(x)
   if (m == 0) {
@@ -18,8 +17,7 @@ slog <- function (x, m = 10)
 }
 
 
-nanmin <- function (Data)
-{
+nanmin <- function (Data){
   if (length(dim(Data)) == 2) {
     SpaltenMinima <- apply(Data, 2, function(x) min(x, na.rm = TRUE))
     SpaltenInd <- NaN
@@ -31,8 +29,7 @@ nanmin <- function (Data)
   return(SpaltenMinima)
 }
 
-nanmax <- function (Data)
-{
+nanmax <- function (Data){
   if (length(dim(Data)) == 2) {
     SpaltenMinima <- apply(Data, 2, function(x) max(x, na.rm = TRUE))
     SpaltenInd <- NaN
@@ -44,8 +41,7 @@ nanmax <- function (Data)
   return(SpaltenMinima)
 }
 
-findAttrCol <- function (AttrName, Header, Data)
-{
+findAttrCol <- function (AttrName, Header, Data){
   i <- which(Header == AttrName)
   if (length(i) == 1) {
     AttrCol = Data[, i]
@@ -58,8 +54,7 @@ findAttrCol <- function (AttrName, Header, Data)
   return(AttrCol)
 }
 
-findAttrInd <- function (AttrName, VarNames)
-{
+findAttrInd <- function (AttrName, VarNames){
   if (is.vector(VarNames)) {
     VarNames <- as.matrix(VarNames)
   }
@@ -84,3 +79,56 @@ findAttrInd <- function (AttrName, VarNames)
     }
   }
 }
+
+
+qquniform <- function (x, Name = "Data", pstyle = 20, filename = "") {
+  x <- x[c(which(!is.nan(x)))]
+  y <- qunif((1:length(x))/length(x), min = min(x), max = max(x))
+  if (filename != "") 
+    postscript(paste(filename, ".eps", sep = ""))
+  qqplot(x, y, xlab = "uniform", ylab = Name, main = "Q/Q Plot compared to uniform distribution", 
+         pch = pstyle, col = "blue")
+  if (filename != "") 
+    dev.off()
+  return(y)
+}
+
+qquniformfit <- function (x, FitSymbol = "p", pstyle = 1, xug = 0, xog = 0, Name = "Data", 
+                          Plot = 1, filename = "") {
+  if (filename != "") 
+    postscript(paste(filename, ".eps", sep = ""))
+  x <- sort(na.last = T, x)
+  y <- qquniform(x, Name, pstyle)
+  z <- lm(y ~ x)
+  abline(z, col = "red", lwd = 2)
+  if (filename != "") 
+    dev.off()
+}
+
+
+qqlognorm <- function (x, VarName = "Data", pstyle = 1, MU, SIGMA) {
+  if (missing(MU)) {
+    if (nanmedian(x) > 0) {
+      MU = log(nanmedian(x))
+    }
+    else {
+      stop("No MU given and median of input data is 0 or less, so no estimation possible")
+    }
+  }
+  if (missing(SIGMA)) {
+    if (mean(x, na.rm = T) > 0) {
+      SIGMA = sqrt(2 * (log(mean(x, na.rm = T)) - MU))
+    }
+    else {
+      stop("No SIGMA given and mean of input data is 0 or less, so no estimation possible")
+    }
+  }
+  n = length(x)
+  RandLogNorm = rlnorm(n, MU, SIGMA)
+  qqplot(x = x, y = RandLogNorm, xlab = cat("LogNorm(", MU, 
+                                            " , ", SIGMA, " )", sep = ""), ylab = VarName, main = "QQplot for LogNormal distribution", 
+         pch = pstyle)
+  return(invisible(list(qqx = x, qqy = RandLogNorm, MU = MU, 
+                        SIGMA = SIGMA)))
+}
+
